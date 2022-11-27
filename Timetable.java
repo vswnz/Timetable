98c2498072ed11002abccb0db8604f4ac32307af
vswnz <vsw@whs.school.nz>
2022-11-27 14:55:50 +1300

    Added timetable slots to master config file

--- Timetable.java
+++ Timetable.java
@@ -18,9 +18,16 @@ import java.io.IOException;  // Handle errors
 public class Timetable
 {
     Scanner keyboard = new Scanner(System.in);
-    final String CONFIGDIR="configs/";
-    final String CONFIGSUF=".timetable";
+    final String CONFIGDIR="configs/";  // directory that configuration files are kept in
+    final String CONFIGSUF=".timetable";  // suffix used by all timetable configuration files
     final String NEWLINE=System.getProperty("line.separator");
+
+    final int MAXCATS=10;   // Maximum number of different categories we can schedule around.
+    int activeCats=0;       // number of categories that are active
+    String[] catNames= new String[MAXCATS];  //Names of each of the categories.
+    
+    final int MAXSLOTS=100; // maximum number of scheduling slots.  
+    int activeSlots=0;   // global state variable that records how many slots the timetable has.
 
     public Timetable()
     {
@@ -69,7 +76,9 @@ public class Timetable
                     break;
                 case "display" : displayCats(TTFile);
                     break;
-                    case "edit" : editCat(TTFile);
+                case "edit" : editCat(TTFile);
+                    break;
+                case "populate" : populateTimeTable(TTFile, TTName);
                     break;
                 case "quit" : stillWorking=false;
                     break;
@@ -86,6 +95,7 @@ public class Timetable
         System.out.println("Add category");
         System.out.println("Display");
         System.out.println("Edit category");
+        System.out.println("Populate a timetable");
         System.out.println("Quit");
     } // print_menu
 
@@ -112,17 +122,15 @@ public class Timetable
         if (!foundAny) System.out.println("No categories defined yet");
     } // displayCats 
 
-    
     // Method to edit a category file.  You can add lines and attributes
     void editCat(File TTFile){
-        
+        System.out.println("Nothing implemented here.");
     } // editCats
-    
+
     // Add an extra category to the config file.  Make the blank category file.
     void addCategory(File TTFile,  String TTName){
         System.out.println("What category do you wish to add to the timetable?");
         String category=keyboard.nextLine();
-
 
         if (categoryExists(TTFile, category)){
             System.out.println(category+" already exists!");
@@ -174,5 +182,63 @@ public class Timetable
 
         return false   ;     
     } // categoryExists
+
+    String[] getSlots(File TTFile){
+        String[] slotsFound = new String[MAXSLOTS];
+        int found=0;
+
+        try{
+            Scanner slotReader = new Scanner(TTFile);
+            while (slotReader.hasNextLine()){ // look though config file for a line that describes a timetable slot
+                String theLine=slotReader.nextLine();
+                String item[]= theLine.split(" ");
+                if (item[0].equals("slot") ) {  // We have a slot line
+                    if (found==MAXSLOTS){
+                        found--;
+                        System.out.println("Error: Too many slots found in config file.  Increase MAXSLOTS and recompile!");
+                        System.out.println("Discarding slot : "+item[1]);
+                    } else {
+                        slotsFound[found]=item[1];
+                        found++;
+                    } // not run off end of array.
+                } // if a slot line has been found
+            } // while there is a next line
+
+        } catch (IOException e) {
+            System.out.println("Error trying to read configuration file for slot information.");
+            e.printStackTrace();
+        } // catch-try
+        activeSlots=found;
+        return slotsFound;        
+    }
+
+    // Populate a timetable
+    // As all timetables must have slots in them, the slots information is kept in the master timetable configuration file.
+    void populateTimeTable(File TTFile, String TTname){  
+    String[] theSlots=getSlots(TTFile);
+    if (activeSlots <1)
+        {
+            System.out.println("You must have slots defined if your config.  This describes the time structure you");
+            System.out.println("want everything timetabled into.  One line per slot, with a name for each.");
+            System.out.println("e.g. If you run a two week timetable (A/B) with 3 classes each day you would have lines");
+            System.out.println("MondayA1 MondayB1 MondayA2 MondayB2 ModayA3 MondayB3 TuesdayA1...FridayB3\n");
+            System.out.println("But if you run a time table with six slots rotating through the week you might just have");
+            System.out.println("slot1 slot2 slot3 slot4 slot5 slot5\n");
+            System.out.println("Remember, one slot to each line of the file!");
+        } else { // go ahead... make my timetable.
+            
+        } // made the timetable.
+    showSlots(theSlots);
+    
+    } // populateTimeTable
+
+    
+    //Display what timetable slots are being used
+    void showSlots(String[] slots){
+        System.out.println("Timetable slots set up:");
+        for (int i=0; i<activeSlots;i++)
+            System.out.println(slots[i]);
+        
+    }
 
 } // Timetable class
